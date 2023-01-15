537ded49655e8008eb14780e160f8bb53131e276
Mario <xmariosalman@gmail.com>
2023-01-15 18:28:46 +0200

    fixed ex 2

--- js/script.js
+++ js/script.js
@@ -2,44 +2,46 @@
 Rezultatele vor fi salvate in variabilele max, respectiv min
 */
 
-let a = 20;
-let b = 10;
-let c = 5;
+// let a = 20;
+// let b = 10;
+// let c = 5;
 
-let min;
-let max;
+// let min;
+// let max;
 
-if (a >= b && a >= c) {
-    max = a;
-} else if (b >= c && b >= a) {
-    max = b;
-} else if (c >= b && c >= a) {
-    max = c;
-}
+// if (a >= b && a >= c) {
+//     max = a;
+// } else if (b >= c && b >= a) {
+//     max = b;
+// } else if (c >= b && c >= a) {
+//     max = c;
+// }
 
-if (a <= b && a <= c) {
-    min = a;
-} else if (b <= a && b <= c) {
-    min = b;
-} else if (c <= a && c <= b) {
-    min = c;
-}
+// if (a <= b && a <= c) {
+//     min = a;
+// } else if (b <= a && b <= c) {
+//     min = b;
+// } else if (c <= a && c <= b) {
+//     min = c;
+// }
 
-console.log("Numarul min este " + min + " Numarul max este " + max);
+// console.log("Numarul min este " + min + " Numarul max este " + max);
 
 /*Sa se calculeze, folosind instructiunea while, suma primelor n numere
 pare numere pare. EX: Pentru n = 3, avem numerele 2,4,6 iar suma lor este 12
 */
 
-const n = 12;
+let n = 3;
 
-let suma = 2;
-let par = 2;
+let sum = 0;
+let i = 2;
 
-while (suma <= n) {
-    console.log(par);
-    par += 2;
-    suma += par;
+while (n > 0) {
+    sum = sum + i;
+    i = i + 2;
+    n = n - 1;
+
+    console.log(sum);
 }
 
 
@@ -47,8 +49,8 @@ while (suma <= n) {
 in ordine descrescatoare
 */
 
-let numar = 0;
+// let numar = 0;
 
-for (let i = 10; i > numar; i--) {
-    console.log(i);
-}
+// for (let i = 10; i > numar; i--) {
+//     console.log(i);
+// }
